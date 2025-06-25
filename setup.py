0b448c0452f39d3dfaa716312045a267e5139a33
Synthfax <synthfax@gmail.com>
2025-06-25 19:58:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,44 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
-    name='webcloner',
-    version='6.25.25.1',
-    author='Synthfax',
-    description='Offline website cloner, updater, and packager',
-    packages=find_packages(),
+    name="webcloner",
+    version="1.0.0",
+    author="Synthfax",
+    author_email="synthfax@gmail.com",  # change to your real or dummy email
+    description="Offline website cloner, updater, and packager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Synthfax/WebCloner",
+    project_urls={
+        "Bug Tracker": "https://github.com/Synthfax/WebCloner/issues",
+    },
+    packages=find_packages(include=["webcloner", "webcloner.*"]),
     install_requires=[
-        'requests',
-        'beautifulsoup4',
-        'tqdm',
-        'flask'
+        "requests>=2.0",
+        "beautifulsoup4>=4.0",
+        "tqdm>=4.0",
+        "flask>=2.0"
     ],
     entry_points={
-        'console_scripts': [
-            'webcloner=webcloner.cloner:main',
-        ]
+        "console_scripts": [
+            "webcloner=webcloner.cloner:main",
+        ],
     },
-    license='Apache License 2.0',
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Utilities",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.8",
+    license="Apache License 2.0",
+    include_package_data=True,
+    zip_safe=False,
 )
+
